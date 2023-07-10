e57ddee7257849a8381bdb233b69da1de69f1fdc
beomsun1234 <beomsun22@gmail.com>
2023-07-10 19:42:05 +0900

    Feat: #1 프로젝트 세팅

---
+++ src/main/java/com/bs/stockpriceservice/StockpriceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bs.stockpriceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpriceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockpriceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bs/stockpriceservice/StockpriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.stockpriceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpriceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
