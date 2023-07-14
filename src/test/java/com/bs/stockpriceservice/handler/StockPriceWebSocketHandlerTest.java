35d3302a11bab2fbe458a5bfd42fb674952f17c9
beomsun1234 <beomsun22@gmail.com>
2023-07-14 20:41:53 +0900

    Test: #3 랜덤 port 추가

--- src/test/java/com/bs/stockpriceservice/handler/StockPriceWebSocketHandlerTest.java
+++ src/test/java/com/bs/stockpriceservice/handler/StockPriceWebSocketHandlerTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StockPriceWebSocketHandlerTest {
 
     @MockBean
