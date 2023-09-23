faa836188f2febcbecd022d95507a8fab2725b2c
beomsun1234 <beomsun22@gmail.com>
2023-09-23 19:16:13 +0900

    feat: 도커이미지

--- src/test/java/com/bs/stockpriceservice/StockpriceServiceApplicationTests.java
+++ src/test/java/com/bs/stockpriceservice/StockpriceServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.bs.stockpriceservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StockpriceServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
